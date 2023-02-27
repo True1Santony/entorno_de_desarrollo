dd40560cf277c8dffe190c0b81aa35f8e545ce65
debian <debian@debian>
2023-02-27 15:20:01 +0100

    3. Introducir el método operativa_cuenta, que englobe las sentencias de la clase Main que operan con el objeto cuenta1.

--- Tarea_ED04/src/cuentas/Main.java
+++ Tarea_ED04/src/cuentas/Main.java
@@ -5,9 +5,7 @@ public class Main {
         CCuenta cuenta1;
         double saldoActual;
 
-        cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
-        saldoActual = cuenta1.estado();
-        System.out.println("El saldo actual es"+ saldoActual );
+        cuenta1 = operativa_cuenta();
 
         try {
             cuenta1.retirar(2300);
@@ -21,4 +19,13 @@ public class Main {
             System.out.print("Fallo al ingresar");
         }
     }
+
+	private static CCuenta operativa_cuenta() {
+		CCuenta cuenta1;
+		double saldoActual;
+		cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        saldoActual = cuenta1.estado();
+        System.out.println("El saldo actual es"+ saldoActual );
+		return cuenta1;
+	}
 }
